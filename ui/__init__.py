2b23c7b1f27aa5c4e9f8c7963b774d5bb4bcda8b
Diogo Silva <dioowdioowtrd@gmail.com>
2026-02-02 23:45:50 -0300

    atalizaçõa 22

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,8 +1,2 @@
-from . import painel
-from . import produtos
-from . import lancamentos
-from . import transferencias
-from . import estoque
-from . import relatorios
-from . import importar_excel
-from . import importar_whatsapp
+
+
